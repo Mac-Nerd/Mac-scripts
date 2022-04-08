874482d1e20aab38ec82c45cb55496b0946cc38f
Charles Mangin - N-able Mac Nerd <95777330+Mac-Nerd@users.noreply.github.com>
2022-04-08 11:04:18 -0400

    SentinelOne monitoring for NC/RMM/Generic

---
+++ Mac-SentinelOne-Status.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# ----- Legal: ----
+# Sample scripts are not supported under any N-able support program or service.
+# The sample scripts are provided AS IS without warranty of any kind.
+# N-able expressly disclaims all implied warranties including, warranties
+# of merchantability or of fitness for a particular purpose.
+# In no event shall N-able or any other party be liable for damages arising
+# out of the use of or inability to use the sample scripts.
+# ----- /Legal ----
+
+# check SentinelOne agent status, append to log file.
+# alert on specific status
+
+SentinelLog="/var/log/sentinelctl.log"
+
+if [ -f "$SentinelLog" ] 
+then
+	eval $(stat -s "$SentinelLog")
+
+	if [ $st_size -gt 1000000 ] # log over 1M? roll it.
+	then
+		LogRename=$(date -r "$SentinelLog" "+%Y-%m-%d-%H%M%S")
+		mv "$SentinelLog" "SentinelLog.$LogRename"
+		touch "$SentinelLog"
+	fi
+else # no log? create one.
+	echo "Creating $SentinelLog"
+	touch "$SentinelLog"
+fi
+
+startTime=$(date)
+
+echo "[ $startTime ]" >> "$SentinelLog"
+
+StatusMessage=$(/usr/local/bin/sentinelctl status --filters agent 2>/dev/null | tee -a "$SentinelLog" ) || echo "[ ERROR ] SentinelOne not installed." | tee -a "$SentinelLog"
+
+ProtectionDisabled="*Protection:*disabled*"
+MissingAuths="*Missing*com.sentinelone*"
+ReadyNo="*Ready:*no*"
+InfectedYes="*Infected:*yes*"
+FWDisabled="*FW*disabled*"
+
+exitcode=0
+
+while IFS= read -r StatusLine
+do
+	case "$StatusLine" in
+
+		$ProtectionDisabled )
+				echo "[ WARNING ] SentinelOne Protection Disabled" | tee -a "$SentinelLog"
+				exitcode=1
+				;;
+		$MissingAuths )
+				echo "[ ERROR ] SentinelOne Missing Authorizations" | tee -a "$SentinelLog"
+				exitcode=2
+				;;
+		$ReadyNo )
+				echo "[ ERROR ] SentinelOne Not Ready" | tee -a "$SentinelLog"
+				exitcode=3
+				;;
+		$FWDisabled )
+				echo "[ ERROR ] Firewall Not Ready" | tee -a "$SentinelLog"
+				exitcode=4
+				;;
+		$InfectedYes )
+				echo "[ ERROR ] INFECTION DETECTED" | tee -a "$SentinelLog"
+				exitcode=255
+				;;		  
+	esac
+done <<-EOF
+$StatusMessage
+EOF
+
+exit $exitcode
