222b042fba6e254a06b590103b97973cd796bfa1
Charles Mangin - Mac Nerd <95777330+Mac-Nerd@users.noreply.github.com>
2022-05-31 19:24:13 -0400

    updated hostname options

--- Mac-Set-Hostname-NetBIOS-Name.sh
+++ Mac-Set-Hostname-NetBIOS-Name.sh
@@ -8,16 +8,23 @@
 # out of the use of or inability to use the sample scripts.
 # ----- /Legal ----
 
-# Set the hostname/ComputerName based on model ID and serial number instead of "username's MacBook":
 
-# get the model ID without commas (eg, 'MacBookPro161')
-ModelID=$(system_profiler SPHardwareDataType | awk '/Identifier/ {print $3}' | sed 's/,//g')
+if [ -n "$1" ]
+then
+	MacName=$(echo "$1" | tr -cd "[:alnum:]")
+else
+	# Set the hostname/ComputerName based on model ID and serial number instead of "username's MacBook":
 
-# get last 4 digits of the serial number
-SerialNumber=$(system_profiler SPHardwareDataType | awk '/Serial/ {print substr($4,length($4)-3,4)}')
+	# get the model ID without commas (eg, 'MacBookPro161')
+	ModelID=$(system_profiler SPHardwareDataType | awk '/Identifier/ {print $3}' | sed 's/,//g')
 
-scutil --set HostName $ModelID-$SerialNumber.local
-scutil --set LocalHostName $ModelID-$SerialNumber.local
-scutil --set ComputerName "$ModelID $SerialNumber"
-/usr/bin/defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName $ModelID-$SerialNumber
+	# get last 4 digits of the serial number
+	SerialNumber=$(system_profiler SPHardwareDataType | awk '/Serial/ {print substr($4,length($4)-3,4)}')
+	MacName="$ModelID$SerialNumber"
+fi
+
+scutil --set HostName "$MacName"
+scutil --set LocalHostName "$MacName"
+scutil --set ComputerName "$MacName"
+/usr/bin/defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName "$MacName"
 dscacheutil -flushcache
