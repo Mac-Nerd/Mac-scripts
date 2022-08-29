192b260c073850fce8383de6db27707f36974795
Charles Mangin - Mac Nerd <95777330+Mac-Nerd@users.noreply.github.com>
2022-08-29 14:05:51 -0400

    SIP check script

---
+++ Mac-SIP-Check.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# ----- Legal: ----
+# Sample scripts are not supported under any N-able support program or service.
+# The sample scripts are provided AS IS without warranty of any kind.
+# N-able expressly disclaims all implied warranties including, warranties
+# of merchantability or of fitness for a particular purpose.
+# In no event shall N-able or any other party be liable for damages arising
+# out of the use of or inability to use the sample scripts.
+# -----------------------------------------------------------
+
+
+# System Integrity Protection (SIP) is integral to the security of macOS. The system
+# prevents unauthorized tampering with protected files in various locations on the boot
+# volume. Disabling SIP is not recommended, and requires physical access to the machine
+# and specific intent to remove the security it provides. It's not something that can be
+# done accidentally. 
+#
+# https://developer.apple.com/documentation/security/disabling_and_enabling_system_integrity_protection
+# 
+# This script will alert if SIP has been disabled on the Mac, and immediately enable it,
+# which requires rebooting. Use with caution, as a sudden reboot can result in the user
+# losing any unsaved work.
+
+# Thanks to Mike Lambert from the MacAdmins Slack for the inspiration and collaboration.
+
+
+
+OSVERSION=$(defaults read /System/Library/CoreServices/SystemVersion ProductVersion | awk '{print $1}')
+
+OSMAJOR=$(echo "${OSVERSION}" | cut -d . -f1)
+OSMINOR=$(echo "${OSVERSION}" | cut -d . -f2)
+
+if [[ $OSMAJOR -lt 11 ]] && [[ $OSMINOR -lt 11 ]] # don't run on anything less than 10.11
+then
+	echo "[ERROR] SIP is only compatible with El Capitan (macOS 10.11) and newer."
+	exit 1002
+fi
+
+
+
+RestartTimeout=30
+
+SIPResult=$(csrutil status | awk '{ print $5 }' | tr -d '.')
+
+if [[ "$SIPResult" == "enabled" ]]; then
+	echo "SIP is already enabled. Nothing to do."
+	exit 0
+
+else
+
+	echo "[WARNING]: SIP has been disabled. Enabling SIP and restarting now."
+	/usr/bin/csrutil enable
+
+osascript <<EOF
+tell application "System Events" to set quitapps to name of every application process whose visible is true and name is not "Finder"
+repeat with closeall in quitapps
+	try
+		with timeout of ${RestartTimeout} seconds
+			quit application closeall
+		end timeout
+	end try
+end repeat
+with timeout of ${RestartTimeout} seconds
+	tell application "Finder" to restart
+end timeout
+EOF
+ 	shutdown -r now
+
+	exit 1001
+
+fi
